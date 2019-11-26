76601d31f111a59a1d04ed03c2379c0389333ce6
Juan Sebastián Montoya <juansmm@outlook.com>
2019-11-26 12:46:58 -0500

    Added Camera Follow Script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+
+    public float boundLeft = 0;
+
+    public float boundRight = 100;
+
+    private void Start()
+    {
+        if (!player)
+        {
+            Debug.LogError("Player not assigned to CameraFollow");
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, boundLeft, boundRight), transform.position.y, transform.position.z);
+    }
+}
