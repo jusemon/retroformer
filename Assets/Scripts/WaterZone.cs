4e7dea2cc4bf08d2b6f2afe26f48a43ddeb0fe11
Juan Sebastián Montoya Montoya <juansmm@outlook.com>
2019-11-25 22:21:51 -0500

    Added WaterZone and WinningZone

---
+++ Assets/Scripts/WaterZone.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class WaterZone : MonoBehaviour
+{
+    [SerializeField]
+    private float timeDelay = 1.0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player enters within the Water Zone, then restart the scene 
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(restartScene());
+        }
+    }
+
+    private IEnumerator restartScene()
+    {
+        // TODO: Show a gameover message (left as exercise)
+
+        // Wait timeDelay 
+        yield return new WaitForSeconds(timeDelay);
+        // Restart Scene 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/WinningZone.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider2D))]
+public class WinningZone : MonoBehaviour
+{
+    [SerializeField]
+    private float timeDelay = 1.0f;
+
+    [SerializeField]
+    private string nextLevelToLoad = "Level2";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the player enters within the Water Zone, then restart the scene 
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(loadNextLevel());
+        }
+    }
+
+
+    private IEnumerator loadNextLevel()
+    {
+        // TODO: Show a winning message (left as exercise) 
+
+        // Wait timeDelay 
+        yield return new WaitForSeconds(timeDelay);
+
+
+        // Load the level named as in the nextLevelToLoad variable. 
+        SceneManager.LoadScene(nextLevelToLoad);
+    }
+
+
+}
